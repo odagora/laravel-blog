92ba348f14b33f767f36c21122e021f09b768b80
ogonzalez29 <oscar.d.gonzalez@gmail.com>
2018-04-16 16:13:28 -0500

    changed blade structure to work with partials

--- resources/views/_includes/nav/main.blade.php
+++ resources/views/_includes/nav/main.blade.php
@@ -1,1 +1,45 @@
-main.blade.php
+<nav class="nav has-shadow">
+    <div class="container">
+        <div class="nav-left">
+            <a class="nav-item is-paddingless" href="{{route('home')}}">
+                <img src="{{asset('images/devmarketer-logo.png')}}" alt="Laravel logo">
+            </a>
+            <a href="#" class="nav-item is-tab is-hidden-mobile m-l-10">Learn</a>
+            <a href="#" class="nav-item is-tab is-hidden-mobile">Discuss</a>
+            <a href="#" class="nav-item is-tab is-hidden-mobile">Share</a>
+        </div>
+        <span class="nav-toggle">
+            <span></span>
+            <span></span>
+            <span></span>
+        </span>
+        <div class="nav-right nav-menu" style="overflow: visible;">
+            <a class="nav-item is-tab is-hidden-tablet is-active">Learn</a>
+            <a class="nav-item is-tab is-hidden-tablet">Discuss</a>
+            <a class="nav-item is-tab is-hidden-tablet">Share</a>
+            @if(Auth::guest())
+                <a href="{{route('login')}}" class="nav-item is-tab">Login</a>
+                <a href="{{route('register')}}" class="nav-item is-tab">Join the Community</a>
+            @else
+                <button class="dropdown is-aligned-right nav-item is-tab">
+                    Hey {{Auth::user()->name}} <span class="icon"><i class="fa fa-caret-down"></i></span>
+                    <ul class="dropdown-menu" style="overflow: visible;">
+                        <li><a href="#">
+                            <span class="icon"><i class="fa fa-fw m-r-10 fa-user-circle-o"></i></span>Profile</a>
+                        </li>
+                        <li><a href="#">
+                            <span class="icon"><i class="fa fa-fw m-r-10 fa-bell"></i></span>Notifications</a>
+                        </li>
+                        <li><a href="#">
+                            <span class="icon"><i class="fa fa-fw m-r-10 fa-cog"></i></span>Manage</a></li>
+                        <li class ="separator"></li>
+                        <li><a href="{{route('logout')}}" onclick="event.preventDefault(); document.getElementbyId('logout.form').submit()">
+                          <span class="icon"><i class="fa fa-fw m-r-10 fa-sign-out"></i></span>Logout</a>
+                          @include('_includes.forms.logout')
+                        </li>
+                    </ul>
+                </button>
+            @endif
+        </div>
+    </div>
+</nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,15 +81,8 @@
                 <div class="title m-b-md">
                     Laravel
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
             </div>
         </div>
+        <script src="{{asset('js/app.js')}}" type="text/javascript"></script>
     </body>
 </html>
