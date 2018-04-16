03fd97b9dcc247ca797731177e7c33a47458ec64
ogonzalez29 <oscar.d.gonzalez@gmail.com>
2018-04-16 15:54:28 -0500

    added main register/login pages and laratrust

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -5,5 +5,17 @@ window.Vue = require('vue');
 import Buefy from 'buefy';
 Vue.use(Buefy);
 
-
 // Vue.component('example', require('./components/Example.vue'));
+
+//Vue object creation to work in blade templates.
+var app = new Vue({
+	el: '#app',
+	data: {}
+})
+
+$(document).ready(function(){
+	//Dropdowns
+	$('button.dropdown').hover(function(e){
+		$(this).toggleClass('is-open');
+	})
+});
